3767deb0e12925a8a76b5eac869e20daf14f7c7c
paulhybryant <18610923953@163.com>
2023-03-09 17:01:35 +0800

    jsl

--- examples/jsl.py
+++ examples/jsl.py
@@ -14,11 +14,10 @@ def toImg(df: pd.DataFrame, img: str):
     # styled dataframe not working for matplotlib
     # need to install google-chrome for the following to work
     dfi.export(df, img, table_conversion='chrome')
-    utils.notify_image_wechat(img)
 
 
 def toImg2(df: pd.DataFrame, img: str, fill_colors = None):
-    fig = df2img.plot_dataframe(df, fig_size=(1920, 800), tbl_cells=dict(fill_color=fill_colors))
+    fig = df2img.plot_dataframe(df, fig_size=(1920, 800), tbl_cells=dict(fill_color=fill_colors), show_fig=False)
     df2img.save_dataframe(fig=fig, filename=img)
 
 
@@ -31,16 +30,23 @@ try:
     img = '/tmp/tmp.png'
     column_fill_colors = []
     color_generator = {
-        '强赎计数': partial(utils.highlight_qs_count, 5, False),
-        '强赎触发价差': partial(utils.highlight_qs_price, False),
-    }
-    for col in df.columns:
+        '强赎计数': lambda v: 'yellow' if v <= 5 else 'green',
+        '强赎触发价差': lambda v: 'pink' if v > 0 else 'white',
+        '涨跌幅': lambda v: 'red' if v > 0 else 'green',
+        '正股涨跌': lambda v: 'red' if v > 0 else 'green',
+        }
+    for col in df.reset_index().columns:
         if col in color_generator:
-            column_fill_colors.append(color_generator.get(col)(df[col]))
+            column_fill_colors.append(utils.highlight_column(False, df[col], color_generator.get(col)))
         else:
             column_fill_colors.append(['white'] * n)
 
+    df['转股溢价率'] = df['转股溢价率'].apply(lambda v: f'{v}%')
+    df['涨跌幅'] = df['涨跌幅'].apply(lambda v: f'{v}%')
+    df['正股涨跌'] = df['正股涨跌'].apply(lambda v: f'{v}%')
+    df['强赎触发价差'] = df['强赎触发价差'].apply(lambda v: f'{v:.2%}')
     toImg2(df, img, column_fill_colors)
+    utils.notify_image_wechat(img)
     print(img, end='')
 except Exception as e:
     utils.notify_text_wechat(str(e))
